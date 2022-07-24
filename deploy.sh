6fa7a36fdc2f53150aebc1898b1ba31f31c108a2
devops1984 <53012880+devops1984@users.noreply.github.com>
2022-07-24 17:45:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@
 echo "Hello"
 sudo docker login
 sudo docker build -t k2r2t2/demoapp .
-sudo docker tag demoapp k2r2t2/demoapp:latest
-sudo docker run -d -p 8003:8080 k2r2t2/demoapp
+docker run -d --name mytomcat -p 8080:8080 k2r2t2/demoapp:latest
+docker exec -it mytomcat /bin/bash
+mv webapps webapps2
+mv webapps.dist/ webapps
+exit
