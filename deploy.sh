815b4ab9202c45ffaf2ebaaf1185af0152e30a98
devops1984 <53012880+devops1984@users.noreply.github.com>
2022-07-28 15:22:31 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Hello"
-sudo docker login
-sudo docker build -t k2r2t2/demoapp .
-docker run -d --name mytomcat -p 8080:8080 k2r2t2/demoapp:latest
-docker exec -it mytomcat /bin/bash
-mv webapps webapps2
-mv webapps.dist/ webapps
-exit
